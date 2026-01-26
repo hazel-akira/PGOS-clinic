f10bd6882b94a60180225426285a8a3ebf73649b
Eivakariuki <evalyne.kariuki@pioneergirlsschool.co.ke>
2026-01-26 15:03:42 +0300

    edit brandname

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandName('PGOS ADMIN')
             ->colors([
                 'primary' => Color::Amber,
             ])
